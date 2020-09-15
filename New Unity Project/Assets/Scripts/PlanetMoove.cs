32b87c5c13afc6d313cd930989fc3176fbdc3be4
LudmilaSavchenko <ludmila.sav.96@gmail.com>
2020-09-15 08:21:26 +0300

    v.02
    сделано:
    UI перенесен в GameManager
    Singleto сделан для ShipMovenet, GameManager, PlanetMoove

---
+++ New Unity Project/Assets/Scripts/PlanetMoove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetMoove : MonoBehaviour
+{
+    private float startZPosition;
+    public static PlanetMoove Instance { get; set; }
+
+    void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+        //Запоминаем позицию по z, чтобы было постоянно расстояние между кораблем и планетой
+        startZPosition = transform.position.z;
+    }
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, ShipMovement.Instance.transform.position.z + startZPosition);
+    }
+
+    public void restartPlanet()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, startZPosition);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/StartGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour 
+{
+    // Update is called once per frame
+    [System.Obsolete]
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            
+            Application.LoadLevel("play");
+        }
+    }
+
+}
